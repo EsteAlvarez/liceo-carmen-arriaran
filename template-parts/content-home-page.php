7f17fbeba2ce36df04b7560b93b7aebd1a606a10
estealvarez <este.alvarez@duocuc.cl>
2023-11-23 01:20:29 -0300

    Agregada Sección de Preguntas Frecuentes

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -11,7 +11,7 @@
 
 <section id="post-<?php the_ID(); ?>" <?php post_class('container'); ?>>
     <!--Liceo carmen arriarán-->
-    <article class="row mb-5 mx-auto">
+    <article class="row mb-5 mx-auto mt-5">
 		<div class="col-lg-12 text-center p-lg-0 p-2 mb-5">
 			<h2><?php the_field('titulo_primera_seccion');?></h2>
 			<div class="d-flex justify-content-center align-items-center">
@@ -74,13 +74,63 @@
             <div class="col-lg-5 col-md-12 text-center p-4 fondo-napsis d-flex justify-content-center align-items-center flex-column">
                 <h3 class="mb-4"><?php the_field('titulo_napsis');?></h3>
                 <p class="mb-4"><?php the_field('descripcion_napsis');?></p>
-                <a class="boton-napsis p-2 pe-4 ps-4 rounded" href="#">Ir a Sitio</a>
+                <a class="boton-napsis p-2 pe-4 ps-4 rounded" href="#">Ir al Sitio</a><!--Agregar link-->
             </div>
         </div>
     </article>
     <!--Napsis-->
 
-    <!--Napsis-->
-    <!--  -->
-    <!--Napsis-->
+    <!--Preguntas Frecuentes-->
+    <article class="row mx-auto mb-5">
+		<div class="col-lg-12 text-center p-lg-0 p-2 mb-5">
+			<h2><?php the_field('titulo_seccion_tres');?></h2>
+			<div class="d-flex justify-content-center align-items-center">
+				<span class="linea-roja-encabezados"></span>
+				<i class="fa-solid fa-graduation-cap ms-3 me-3"></i>
+				<span class="linea-roja-encabezados"></span>
+			</div>
+		</div>
+
+		<div class="col-lg-6">
+			<div class="accordion">
+				<div class="accordion-elemento mb-2">
+					<div class="titulo-accordion"><?php the_field('primera_pregunta');?></div>
+					<div class="contenido-accordion"><?php the_field('contenido_primera_pregunta');?></div>
+				</div>
+				<div class="accordion-elemento mb-2">
+					<div class="titulo-accordion"><?php the_field('segunda_pregunta');?></div>
+					<div class="contenido-accordion"><?php the_field('contenido_segunda_pregunta');?></div>
+				</div>
+				<div class="accordion-elemento mb-2">
+					<div class="titulo-accordion"><?php the_field('tercera_pregunta');?></div>
+					<div class="contenido-accordion"><?php the_field('contenido_tercera_pregunta');?></div>
+				</div>
+				<div class="accordion-elemento mb-2">
+					<div class="titulo-accordion"><?php the_field('cuarta_pregunta');?></div>
+					<div class="contenido-accordion"><?php the_field('contenido_cuarta_pregunta');?></div>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-6">
+			<div class="accordion">
+				<div class="accordion-elemento mb-2">
+					<div class="titulo-accordion"><?php the_field('quinta_pregunta');?></div>
+					<div class="contenido-accordion"><?php the_field('contenido_quinta_pregunta');?></div>
+				</div>
+				<div class="accordion-elemento mb-2">
+					<div class="titulo-accordion"><?php the_field('sexta_pregunta');?></div>
+					<div class="contenido-accordion"><?php the_field('contenido_sexta_pregunta');?></div>
+				</div>
+				<div class="accordion-elemento mb-2">
+					<div class="titulo-accordion"><?php the_field('septima_pregunta');?></div>
+					<div class="contenido-accordion"><?php the_field('contenido_septima_pregunta');?></div>
+				</div>
+				<div class="accordion-elemento mb-2">
+					<div class="titulo-accordion"><?php the_field('octava_pregunta');?></div>
+					<div class="contenido-accordion"><?php the_field('contenido_octava_pregunta');?></div>
+				</div>
+			</div>
+		</div>
+    </article>
+    <!--Preguntas Frecuentes-->
 </section><!-- #post-<?php the_ID(); ?> -->
