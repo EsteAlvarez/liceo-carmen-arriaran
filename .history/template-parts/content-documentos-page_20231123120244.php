391d27ad99fb7bd6f9b97c5111b396f8e2138b68
katherine-estrada <ka.estrada@duocuc.cl>
2023-11-23 15:36:27 -0300

    cambios

--- .history/template-parts/content-documentos-page_20231123120244.php
+++ .history/template-parts/content-documentos-page_20231123120244.php
@@ -11,14 +11,14 @@
 
 <section id="post-<?php the_ID(); ?>" <?php post_class('container'); ?>>
 	<article>
-			<div class="col-lg-12 text-center p-lg-0 p-2 mb-5">
-				<h2><?php the_field('titulo_documentos');?></h2>
-					<div class="d-flex justify-content-center align-items-center">
-						<span class="linea-roja-encabezados"></span>
-						<i class="fa-solid fa-graduation-cap ms-3 me-3"></i>
-						<span class="linea-roja-encabezados"></span>
-					</div>
-			</div>
+		<div class="col-lg-12 text-center p-lg-0 p-2 mb-5">
+			<h2><?php the_field('titulo_documentos');?></h2>
+				<div class="d-flex justify-content-center align-items-center">
+					<span class="linea-roja-encabezados"></span>
+					<i class="fa-solid fa-graduation-cap ms-3 me-3"></i>
+					<span class="linea-roja-encabezados"></span>
+				</div>
+		</div>
 		<div>
 			<table class="table table-dark table-hover caption-top">
 			<caption>Documentos</caption>
