6e5c832484b15d6ea4e3e677eb968185b006435c
estealvarez <este.alvarez@duocuc.cl>
2023-11-23 16:22:32 -0300

    Update content-home-page.php

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -11,7 +11,7 @@
 
 <section id="post-<?php the_ID(); ?>" <?php post_class('container-fluid'); ?>>
 	<!--Carrusel-->
-	<article>
+	<article class="row">
 		<div id="carouselExampleCaptions" class="carousel slide">
 			<div class="carousel-indicators">
 				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
