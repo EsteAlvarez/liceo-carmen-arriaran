aad7cc46aa76b2b5d0c8f6036ef39f5e5c91839b
estealvarez <este.alvarez@duocuc.cl>
2023-11-23 16:46:48 -0300

    Update content-home-page.php

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -14,7 +14,7 @@
 	<article class="row">
 		<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
 			<div class="carousel-inner">
-				<div class="carousel-item active" data-bs-interval="10000">
+				<div class="carousel-item active" data-bs-interval="5000">
 					<?php 
 					$image = get_field('primera_imagen');
 					if( !empty( $image ) ): ?>
@@ -24,7 +24,7 @@
 						<h5><?php the_field('texto_primera_imagen');?></h5>
 					</div>
 				</div>
-				<div class="carousel-item" data-bs-interval="2000">
+				<div class="carousel-item" data-bs-interval="5000">
 					<?php 
 						$image = get_field('segunda_imagen');
 						if( !empty( $image ) ): ?>
@@ -54,37 +54,6 @@
 				<span class="visually-hidden">Next</span>
 			</button>
 		</div>
-
-
-
-
-
-		<!-- <div id="carouselExampleAutoplaying" class="carousel slide border">
-			<div class="carousel-indicators">
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-			</div>
-			<div class="carousel-inner">
-				<div class="carousel-item active" data-bs-interval="5000">
-					
-				</div>
-			<div class="carousel-item" data-bs-interval="5000">
-					
-			</div>
-			<div class="carousel-item" data-bs-interval="5000">
-				
-			</div>
-			</div>
-			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Previous</span>
-			</button>
-			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Next</span>
-			</button>
-		</div> -->
 	</article>
 	<!--Carrusel-->
     <!--Liceo carmen arriarán-->
