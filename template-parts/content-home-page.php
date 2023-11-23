e2175e3a3e3bce77a8f84f5b63193a5e6314f84c
estealvarez <este.alvarez@duocuc.cl>
2023-11-23 16:17:29 -0300

    Carrusel
    Se agrego carrusel a la página de inicio

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -10,6 +10,57 @@
 ?>
 
 <section id="post-<?php the_ID(); ?>" <?php post_class('container'); ?>>
+	<!--Carrusel-->
+	<article class="row">
+		<div id="carouselExampleCaptions" class="carousel slide">
+			<div class="carousel-indicators">
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+			</div>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<?php 
+					$image = get_field('primera_imagen');
+					if( !empty( $image ) ): ?>
+						<img class="img-fluid" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+					<?php endif; ?>
+					<div class="carousel-caption d-none d-md-block">
+						<h5><?php the_field('texto_primera_imagen');?></h5>
+					</div>
+				</div>
+			<div class="carousel-item">
+					<?php 
+					$image = get_field('segunda_imagen');
+					if( !empty( $image ) ): ?>
+						<img class="img-fluid" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+					<?php endif; ?>
+				<div class="carousel-caption d-none d-md-block">
+					<h5><?php the_field('texto_segunda_imagen');?></h5>
+				</div>
+			</div>
+			<div class="carousel-item">
+					<?php 
+					$image = get_field('tercera_imagen');
+					if( !empty( $image ) ): ?>
+						<img class="img-fluid" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+					<?php endif; ?>
+				<div class="carousel-caption d-none d-md-block">
+					<h5><?php the_field('texto_tercera_imagen');?></h5>
+				</div>
+			</div>
+			</div>
+			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+	</article>
+	<!--Carrusel-->
     <!--Liceo carmen arriarán-->
     <article class="row mb-5 mx-auto mt-5">
 		<div class="col-lg-12 text-center p-lg-0 p-2 mb-5">
