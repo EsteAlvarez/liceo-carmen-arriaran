9d096f871b5c495f58909977d4b65d26f105fa5d
estealvarez <este.alvarez@duocuc.cl>
2023-11-23 16:44:34 -0300

    Update content-home-page.php

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -12,14 +12,9 @@
 <section id="post-<?php the_ID(); ?>" <?php post_class('container-fluid p-0 m-0'); ?>>
 	<!--Carrusel-->
 	<article class="row">
-		<div id="carouselExampleAutoplaying" class="carousel slide border">
-			<div class="carousel-indicators">
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-			</div>
+		<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
 			<div class="carousel-inner">
-				<div class="carousel-item active" data-bs-interval="5000">
+				<div class="carousel-item active" data-bs-interval="10000">
 					<?php 
 					$image = get_field('primera_imagen');
 					if( !empty( $image ) ): ?>
@@ -29,25 +24,56 @@
 						<h5><?php the_field('texto_primera_imagen');?></h5>
 					</div>
 				</div>
-			<div class="carousel-item" data-bs-interval="5000">
+				<div class="carousel-item" data-bs-interval="2000">
 					<?php 
-					$image = get_field('segunda_imagen');
-					if( !empty( $image ) ): ?>
-						<img class="img-fluid imagen-carrusel" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-					<?php endif; ?>
-				<div class="carousel-caption d-none d-md-block">
-					<h5><?php the_field('texto_segunda_imagen');?></h5>
+						$image = get_field('segunda_imagen');
+						if( !empty( $image ) ): ?>
+							<img class="img-fluid imagen-carrusel" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+						<?php endif; ?>
+					<div class="carousel-caption d-none d-md-block">
+						<h5><?php the_field('texto_segunda_imagen');?></h5>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<?php 
+						$image = get_field('tercera_imagen');
+						if( !empty( $image ) ): ?>
+							<img class="img-fluid imagen-carrusel" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+						<?php endif; ?>
+					<div class="carousel-caption d-none d-md-block">
+						<h5><?php the_field('texto_tercera_imagen');?></h5>
+					</div>
 				</div>
 			</div>
-			<div class="carousel-item" data-bs-interval="5000">
-					<?php 
-					$image = get_field('tercera_imagen');
-					if( !empty( $image ) ): ?>
-						<img class="img-fluid imagen-carrusel" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-					<?php endif; ?>
-				<div class="carousel-caption d-none d-md-block">
-					<h5><?php the_field('texto_tercera_imagen');?></h5>
+			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+
+
+
+
+
+		<!-- <div id="carouselExampleAutoplaying" class="carousel slide border">
+			<div class="carousel-indicators">
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+				<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+			</div>
+			<div class="carousel-inner">
+				<div class="carousel-item active" data-bs-interval="5000">
+					
 				</div>
+			<div class="carousel-item" data-bs-interval="5000">
+					
+			</div>
+			<div class="carousel-item" data-bs-interval="5000">
+				
 			</div>
 			</div>
 			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
@@ -58,7 +84,7 @@
 				<span class="carousel-control-next-icon" aria-hidden="true"></span>
 				<span class="visually-hidden">Next</span>
 			</button>
-		</div>
+		</div> -->
 	</article>
 	<!--Carrusel-->
     <!--Liceo carmen arriarán-->
