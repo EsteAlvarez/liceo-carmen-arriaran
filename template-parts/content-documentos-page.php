2cf320a0ad01821a333dce00e3331f3e14dece61
estealvarez <este.alvarez@duocuc.cl>
2023-11-24 14:01:59 -0300

    Update content-documentos-page.php

--- template-parts/content-documentos-page.php
+++ template-parts/content-documentos-page.php
@@ -11,6 +11,7 @@
 
 <section id="post-<?php the_ID(); ?>" <?php post_class('container'); ?>>
 	<article>
+		<div><?php echo the_content();?></div>
 			<div class="col-lg-12 text-center p-lg-0 p-2 mb-5">
 				<h2><?php the_field('titulo_documentos');?></h2>
 					<div class="d-flex justify-content-center align-items-center">
