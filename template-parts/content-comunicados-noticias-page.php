fcdb890c97b31905c2134d672d40f510443b4ec0
estealvarez <este.alvarez@duocuc.cl>
2023-11-26 19:42:32 -0300

    Update content-comunicados-noticias-page.php

--- template-parts/content-comunicados-noticias-page.php
+++ template-parts/content-comunicados-noticias-page.php
@@ -10,7 +10,10 @@
 ?>
 
 <section id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<div>
-<?php include get_template_directory() . '/assets/modulos/modulo-comunicados/loop-comunicados.php';?>
-</div>
+	<div>
+	<?php include get_template_directory() . '/assets/modulos/modulo-comunicados/loop-comunicados.php';?>
+	</div>
+	<div>
+	<?php include get_template_directory() . '/assets/modulos/modulo-noticias/loop-noticias.php';?>
+	</div>
 </section><!-- #post-<?php the_ID(); ?> -->
