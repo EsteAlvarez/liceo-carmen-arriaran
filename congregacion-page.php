579c3a44fb0f8811ed1976df16d1505154059fe0
VivianaUyarte <viv.berrios@duocuc.cl>
2023-11-27 01:27:37 -0300

    congregación
    avance congregación, falta la info

--- congregacion-page.php
+++ congregacion-page.php
@@ -15,8 +15,55 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
+<h2 class="d-flex text-center justify-content-center">Formación del Liceo</h2>
+<div class="d-flex justify-content-center align-items-center">
+	<span class="linea-roja-encabezados"></span>
+	<i class="fa-solid fa-graduation-cap ms-3 me-3"></i>
+	<span class="linea-roja-encabezados"></span>
+</div>
 
+<section class="col-md-12 row d-flex gap-3 justify-content-center mt-3">
+<div class="card col-8 col-md-3">
+kdsjhdklj
+djkkjdkjdj
+</div>
+<div class="card col-8 col-md-3">
+kdsjhdklj
+djkkjdkjdj
+</div>
+<div class="card col-8 col-md-3">
+kdsjhdklj
+djkkjdkjdj
+</div>
+</section>
+
+<section class="container d-flex  align-items-center justify-content-center ">
+	
+<div class="col-2 col-md-2 pLineRed "></div>
+	<i class="bi bi-circle-fill"></i>
+<div class="col-md-2 pLineRed"></div>
+	<i class="bi bi-circle-fill"></i>
+<div class="col-md-2 pLineRed"></div>
+	<i class="bi bi-circle-fill"></i>
+<div class="col-md-2 pLineRed"></div>
+	<i class="bi bi-circle-fill"></i>
+<div class="col-md-2 pLineRed"></div>
+<i class="bi bi-circle-fill"></i>
+<div class="col-md-2 pLineRed"></div>
+
+</section>
+
+<section class="col-md-12 row d-flex gap-3 justify-content-center">
+<div class="card col-8 col-md-3">
+kdsjhdklj
+djkkjdkjdj
+</div>
+<div class="card col-8 col-md-3">
+kdsjhdklj
+djkkjdkjdj
+</div>
+</section>
 		<?php
 		while ( have_posts() ) :
 			the_post();
