54f97523e03a2fa89ef2bfdb05f7fab6bbdce0c2
estealvarez <este.alvarez@duocuc.cl>
2023-11-28 00:17:24 -0300

    Update content-single-comunicados.php

--- template-parts/content-single-comunicados.php
+++ template-parts/content-single-comunicados.php
@@ -29,7 +29,7 @@
    	    </div>
 	</div>
 	<div class="mt-5 mb-5 container-fluid scroll">
-		<div class="row">
+		<div class="row m-0 p-0">
 			<div class="col-lg-8 col-md-12">
 				<h3 class="comunicados-single"><?php the_title();?></h3>
 				<p class="mt-5"><?php the_content();?></p>
