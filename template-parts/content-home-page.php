856292ad35d8c04d61a6347ee6d12af7bb125d3e
estealvarez <este.alvarez@duocuc.cl>
2023-12-04 23:58:29 -0300

    Update content-home-page.php

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -168,9 +168,9 @@
 			<div class="accordion col-lg-6 scroll mb-lg-0 mb-mb-0 mb-3" id="accordionExample">
 				<div class="accordion-item mb-3">
 					<h2 class="accordion-header">
-					<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+					<div class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
 						<span class="fw-bold"><?php the_field('primera_pregunta');?></span>
-					</button>
+					</div>
 					</h2>
 					<div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
 					<div class="accordion-body">
