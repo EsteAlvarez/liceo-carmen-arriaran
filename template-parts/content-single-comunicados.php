957854be469d4580f6f170a453f948fe0f4039c2
estealvarez <este.alvarez@duocuc.cl>
2023-12-05 01:20:08 -0300

    corrección de estilos

--- template-parts/content-single-comunicados.php
+++ template-parts/content-single-comunicados.php
@@ -30,11 +30,11 @@
 	</article>
 	<article class="mt-5 mb-5 container scroll">
 		<div class="row m-0 p-0">
-			<div class="col-lg-8 col-md-12 contenido-principal-scomunicados">
+			<div class="col-lg-8 col-md-12">
 				<h2 class="comunicados-single"><?php the_title();?></h2>
 				<p class="mt-5"><?php the_content();?></p>
 			</div>
-			<div class="col-lg-4 col-md-12 mt-lg-0 mt-md-5 mt-5 p-2">
+			<div class="col-lg-4 col-md-12 mt-lg-0 mt-md-5 mt-5">
 				<div>
 					<span class="comunicados-single fw-bold">Remitente</span>
 					<?php
