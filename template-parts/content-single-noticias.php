c3ee3d9255cd6b2f0d12146f21c78388ddd0912b
estealvarez <este.alvarez@duocuc.cl>
2023-12-06 01:21:36 -0300

    Sección más noticias de single noticias

--- template-parts/content-single-noticias.php
+++ template-parts/content-single-noticias.php
@@ -43,7 +43,74 @@
 	<article class="mas-noticias container mb-5">
 		<div class="row">
 			<h2 class="comunicados-single fw-lighter mb-3"><?php the_field('titulo_de_seccion_extra');?></h2>
-			<?php include get_template_directory() . '/assets/modulos/modulo-noticias/loop-noticias.php';?>
+			<div class="row">
+			<script>
+			function incrustar_hoja_estilos_noticias() {
+				var hoja_estilos_url = '<?php echo get_site_url() . '/wp-content/themes/carmen-arriaran/assets/modulos/modulo-noticias/modulo-noticias.css';?>';
+				var hoja_estilos = document.createElement('link');
+				hoja_estilos.rel = 'stylesheet';
+				hoja_estilos.href = hoja_estilos_url;
+				document.head.appendChild(hoja_estilos);
+			}
+			incrustar_hoja_estilos_noticias();
+			</script>
+				<!--Custom Loop-->
+				<?php 
+					$current_post_id = get_the_ID(); // Obtener el ID del comunicado actual
+					$temp = $wp_query;
+					$paged = (get_query_var('paged')) ? get_query_var('paged') : 1; //Número de páginas
+					$post_per_page = -1; // -1 muestra todos los posts Cantidad de posts a mostrar
+					$args = array(
+						'post_type' => 'noticias',
+						'orderby' => 'date',
+						'order' => 'ASC', // ASC ascendente
+						'paged' => $paged,
+						'posts_per_page' => $post_per_page,
+						'tax_query' => array(
+							array(
+								'taxonomy' => 'categoria-noticias',
+								'field' => 'slug',
+								'terms' => 'noticias'
+							),
+						),
+						'post__not_in' => array($current_post_id), // Excluir el comunicado actual
+					);
+
+					$wp_query = new WP_Query($args);
+					if ($wp_query->have_posts()) :
+						while ($wp_query->have_posts()) : $wp_query->the_post();
+				?>
+					 <div class="col-lg-4 col-md-6 col-12 mb-lg-0 mb-md-5 mb-5">
+						<div class="card carta-noticias mx-auto h-100 position-relative">
+							<div class="fondo-carta mb-5">
+								<div><?php the_post_thumbnail(); ?></div>
+								<div class="card-body">
+									<?php
+										$mi_campo_fecha = get_field('fecha_de_noticia');
+										if ($mi_campo_fecha) {
+											echo '<p class="fecha-comunicado m-0 mb-1">' . $mi_campo_fecha . '</p>';
+										}
+									?>
+									<h3 class="titulos-comunicados fs-5"><?php the_title();?></h3>
+									<span class="contenido-extracto-noticias"><?php the_excerpt();?></span>
+								</div>
+							</div>
+							<div class="boton-noticias position-absolute bottom-0 start-0 p-3">
+								<a href="<?php the_permalink(); ?>">Ver Mas</a>
+							</div>
+						</div>
+					</div>
+				<?php
+						endwhile;
+						wp_reset_postdata(); // Restablecer datos del post
+					else :
+						// No se encontraron noticias
+						echo 'No se encontraron noticias.';
+					endif;
+					$wp_query = $temp;
+				?>
+				<!--Custom Loop-->
+			</div>
 		</div>
 	</article>
 </section><!-- #post-<?php the_ID(); ?> -->
