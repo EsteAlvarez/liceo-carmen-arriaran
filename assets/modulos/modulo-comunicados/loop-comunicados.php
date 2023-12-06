9d054747348afb7d49e09a47c34ff78b91dadb29
estealvarez <este.alvarez@duocuc.cl>
2023-12-05 23:25:25 -0300

    Update loop-comunicados.php

--- assets/modulos/modulo-comunicados/loop-comunicados.php
+++ assets/modulos/modulo-comunicados/loop-comunicados.php
@@ -36,7 +36,7 @@ incrustar_hoja_estilos_comunicados();
     if (have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
         <div class="comunicado d-flex p-0 mb-5 position-relative">
             <div class="col-lg-3 p-0 m-0 d-lg-block d-md-none d-none contenedor-imagen" style="background-image: url('<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID));?>');"></div>
-            <div class="contenido col-lg-9 p-3 m-0 mb-lg-0 mb-md-5 mb-5">
+            <div class="contenido col-lg-9 p-4 m-0 mb-lg-0 mb-md-5 mb-5">
                 <div class="d-flex flex-wrap justify-content-between">
                     <h3 class="titulos-comunicados fs-5 m-0 mb-1"><?php the_title();?></h3>
                     <?php
