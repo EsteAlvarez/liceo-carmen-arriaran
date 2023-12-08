67205628d980a6b997dabb39ea8dcddc853ccb52
estealvarez <este.alvarez@duocuc.cl>
2023-12-08 00:03:56 -0300

    footer

--- footer.php
+++ footer.php
@@ -13,19 +13,19 @@
 <footer id="colophon" class="site-footer pFondoFooter container-fluid m-0 p-0">
 	<div class="row m-0 p-3">
 		<!--logo-->
-		<div class="col-lg-3 col-md-6 pLiceo text-center d-flex justify-content-center align-items-center flex-column border mb-lg-0 mb-md-3">
+		<div class="col-lg-3 col-md-6 pLiceo text-center d-flex justify-content-center align-items-center flex-column mb-lg-0 mb-md-3">
 			<?php dynamic_sidebar('nav_dos');?>
 		</div>
 		<!--logos napsis-->
-		<div class="col-lg-3 col-md-6 text-center d-flex justify-content-center align-items-center flex-column border mb-lg-0 mb-md-3">
+		<div class="col-lg-3 col-md-6 text-center d-flex justify-content-center align-items-center flex-column mb-lg-0 mb-md-3">
 			<?php dynamic_sidebar('nav_tres');?>
 		</div>
 		<!--info general-->
-		<div class="col-lg-3 col-md-6 col-12 p-lg-3 d-flex justify-content-center align-items-center border ">		
+		<div class="col-lg-3 col-md-6 col-12 p-lg-3 d-flex justify-content-center align-items-center ">		
 			<?php dynamic_sidebar('nav_cuatro');?>
 		</div>
 		<!--mapa-->
-		<div class="col-lg-3 col-md-6 border">
+		<div class="col-lg-3 col-md-6">
 			<div class="pMapaFooter p-2 d-flex flex-column">
 				<iframe class="mapaFooter shadow bg-body-tertiary m-0 mb-2" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d4383.031692726244!2d-70.58587457924969!3d-33.509526594237464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sliceo%20carmen%20arriaran!5e0!3m2!1ses!2scl!4v1700965569070!5m2!1ses!2scl"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 				<span class="pDirFooter">Departamental 4850 - Peñalolen - Metro Macul </span>
